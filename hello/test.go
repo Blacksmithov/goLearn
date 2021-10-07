20a0939b8b1a4702cebd663a238c7a48d770d637
Blacksmithov <maxk@bk.ru>
2021-10-07 20:38:53 +0500

    asdasd

--- hello/test.go
+++ hello/test.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"https://github.com/Blacksmithov/goLearn/tree/main/pack1"
 	"fmt"
 )
 
 func main() {
-		fmt.Println("run test")
-		fmt.Println(		pack1.Tre())
+	fmt.Println("run test")
 }
